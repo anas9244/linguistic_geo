9d4ebf2eb6ea42c0d557c18bd50214dd53ed1944
Anas Alnayef <anas9244@hotmail.com>
2020-02-20 05:56:13 +0100

    cluster_city grapsh

--- order_plt_mat.py
+++ order_plt_mat.py
@@ -16,22 +16,22 @@ new_tweets_dict_file.close()
 names = [i for i in new_tweets_dict]
 
 
-iter_results_file = open("iter_results_merged_new.pickle", "rb")
+iter_results_file = open("iter_results_jsd_cities.pickle", "rb")
 iter_results_Z = pickle.load(iter_results_file)
 iter_results_file.close()
 D_Z = sum(iter_results_Z) / len(iter_results_Z)
-
-iter_results_file = open("iter_results_tfidf_new.pickle", "rb")
+print(len(iter_results_Z))
+iter_results_file = open("iter_results_tfidf_cities.pickle", "rb")
 iter_results_tfidf = pickle.load(iter_results_file)
 iter_results_file.close()
 D_tfidf = sum(iter_results_tfidf) / len(iter_results_tfidf)
+print(len(iter_results_tfidf))
 
-
-iter_results_file = open("iter_results_jsd_merged_new.pickle", "rb")
+iter_results_file = open("iter_results_jsd_cities.pickle", "rb")
 iter_results_jsd = pickle.load(iter_results_file)
 iter_results_file.close()
 D_jsd = sum(iter_results_jsd) / len(iter_results_jsd)
-
+print(len(iter_results_jsd))
 
 noremd_mat = np.zeros((len(D_jsd), len(D_jsd)))
 
@@ -52,24 +52,26 @@ plt.xticks(rotation=0)
 dendo = sns.clustermap(noremd_mat, row_linkage=linkage,
                        col_linkage=linkage, cmap="jet", cbar_pos=(0.09, 0.01, .03, 0.7480577777777779))
 
-leafs = dendo.dendrogram_col.reordered_ind
-cluster_names = []
-for i in leafs:
-    cluster_names.append(names[i])
+# leafs = dendo.dendrogram_col.reordered_ind
+# cluster_names = []
+# for i in leafs:
+#     cluster_names.append(names[i])
 
 
-positions = [i + 0.5 for i in range(len(names))]
+# positions = [i + 0.5 for i in range(len(names))]
 # ax.set_xticklabels(cluster_names)
 
-dendo.ax_heatmap.xaxis.set_ticks(positions)
-dendo.ax_heatmap.xaxis.set_ticklabels(cluster_names)
+# dendo.ax_heatmap.xaxis.set_ticks(positions)
+# dendo.ax_heatmap.xaxis.set_ticklabels(cluster_names)
 
-dendo.ax_heatmap.yaxis.set_ticks(positions)
-dendo.ax_heatmap.yaxis.set_ticklabels(cluster_names)
+# dendo.ax_heatmap.yaxis.set_ticks(positions)
+# dendo.ax_heatmap.yaxis.set_ticklabels(cluster_names)
 
-# dendo.ax_heatmap.invert_yaxis()
-dendo.ax_heatmap.xaxis.set_ticks_position('top')
-dendo.ax_heatmap.yaxis.set_ticks_position('left')
+
+# dendo.ax_heatmap.xaxis.set_ticks_position('top')
+# dendo.ax_heatmap.yaxis.set_ticks_position('left')
+
+dendo.ax_heatmap.axis('off')
 
 
 dendo.ax_row_dendrogram.set_visible(False)
