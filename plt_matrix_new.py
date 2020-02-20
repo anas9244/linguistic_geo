adb08f867d84d0ce2219ba76b4e909a552693125
Anas Alnayef <anas9244@hotmail.com>
2020-02-20 05:10:53 +0100

    city cluster + graph

--- plt_matrix_new.py
+++ plt_matrix_new.py
@@ -11,18 +11,18 @@ new_tweets_dict = pickle.load(new_tweets_dict_file)
 new_tweets_dict_file.close()
 
 
-iter_results_file = open("iter_results_merged_new.pickle", "rb")
+iter_results_file = open("iter_results_jsd_cities.pickle", "rb")
 iter_results_Z = pickle.load(iter_results_file)
 iter_results_file.close()
 D_Z = sum(iter_results_Z) / len(iter_results_Z)
 print(len(iter_results_Z))
-iter_results_file = open("iter_results_tfidf_new.pickle", "rb")
+iter_results_file = open("iter_results_tfidf_cities.pickle", "rb")
 iter_results_tfidf = pickle.load(iter_results_file)
 iter_results_file.close()
 D_tfidf = sum(iter_results_tfidf) / len(iter_results_tfidf)
 print(len(iter_results_tfidf))
 
-iter_results_file = open("iter_results_jsd_merged_new.pickle", "rb")
+iter_results_file = open("iter_results_jsd_cities.pickle", "rb")
 iter_results_jsd = pickle.load(iter_results_file)
 iter_results_file.close()
 D_jsd = sum(iter_results_jsd) / len(iter_results_jsd)
@@ -45,16 +45,16 @@ print(len(noremd_mat))
 
 fig = plt.figure()
 ax = fig.add_subplot()
-cax = ax.matshow(noremd_mat, cmap='jet')
+cax = ax.matshow(D_jsd, cmap='jet')
 fig.colorbar(cax)
 ticks = np.arange(0, len(new_tweets_dict), 1)
-plt.title("The norm of the distance matrix of Z-socres, TF-IDF and JSD, num of states: " +
-          str(len(new_tweets_dict)))
-ax.set_xticks(ticks,)
-ax.set_yticks(ticks)
-ax.set_xticklabels(names, size=7)
-ax.set_yticklabels(names, size=7)
-
+plt.title("JSD dist_map, num of cities > 5000 tweets : " +
+          str(len(noremd_mat)))
+# ax.set_xticks(ticks,)
+# ax.set_yticks(ticks)
+# ax.set_xticklabels(names, size=7)
+# ax.set_yticklabels(names, size=7)
+plt.axis('off')
 
 plt.show()
 
